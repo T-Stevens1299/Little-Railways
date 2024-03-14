6df2b154cb53e8a2d8f8bec48603d0877a396741
T-Stevens1299 <s012473l@student.staffs.ac.uk>
2024-03-14 12:21:33 +0000

    C++ Class fixed

--- Source/LittleRailways/TestWagon.cpp
+++ Source/LittleRailways/TestWagon.cpp
@@ -9,19 +9,19 @@ ATestWagon::ATestWagon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	WagonBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wagon Body"));
+	WagonBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComponent"));
 	WagonBody->SetupAttachment(GetRootComponent());
 
-	LeftWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left Wheel 1"));
+	LeftWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponentA"));
 	LeftWheel1->SetupAttachment(WagonBody);
 
-	LeftWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left Wheel 2"));
+	LeftWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponentB"));
 	LeftWheel2->SetupAttachment(WagonBody);
 
-	RightWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right Wheel 1"));
+	RightWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponentC"));
 	RightWheel1->SetupAttachment(WagonBody);
 
-	RightWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right Wheel 2"));
+	RightWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponentD"));
 	RightWheel2->SetupAttachment(WagonBody);
 }
 
