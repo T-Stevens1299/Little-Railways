bdcb6a342c300e4b3cd81daf794f890559e00156
T-Stevens1299 <91890549+T-Stevens1299@users.noreply.github.com>
2024-04-24 16:38:14 +0100

    Bogie Creation

---
+++ Source/LittleRailways/Private/LocoDrivers.cpp
@@ -0,0 +1,70 @@
+// Copyright Stevens Studios, all rights reserved
+// Written by Thomas Stevens
+
+
+#include "LocoDrivers.h"
+
+// Sets default values
+ALocoDrivers::ALocoDrivers()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FrameBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComponent"));
+	FrameBody->SetupAttachment(GetRootComponent());
+
+	LeftWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponent1"));
+	LeftWheel1->SetupAttachment(FrameBody);
+
+	LeftWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponent2"));
+	LeftWheel2->SetupAttachment(FrameBody);
+
+
+	RightWheel1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponent3"));
+	RightWheel1->SetupAttachment(FrameBody);
+
+	RightWheel2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelComponent4"));
+	RightWheel2->SetupAttachment(FrameBody);
+
+}
+
+// Called when the game starts or when spawned
+void ALocoDrivers::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	for (int i = 0; i < wheelsToDrive.Num(); i++)
+	{
+		wheelsToDrive[i]->SetPhysicsMaxAngularVelocityInRadians(MaxSpeedKph, false);
+	}
+}
+
+// Called every frame
+void ALocoDrivers::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALocoDrivers::ApplyTorque(int passedTorqueMultiplier)
+{
+	for (int i = 0; i < wheelsToDrive.Num(); i++)
+	{
+		wheelsToDrive[i]->AddTorqueInRadians(FVector(0.0f, (passedTorqueMultiplier * TractiveTorque), 0.0f));
+	}
+}
+
+void ALocoDrivers::ApplyBreaks(int passedBrakeVal)
+{
+	float brakeVal = passedBrakeVal;
+
+	for (int i = 0; i < wheelsToDrive.Num(); i++)
+	{
+		wheelsToDrive[i]->SetAngularDamping(brakeVal);
+	}
+}
+
+void ALocoDrivers::SetTrainPtr(ALocoController* TrainPrt)
+{
+	TrainRef = TrainPrt;
+}
---
+++ Source/LittleRailways/Public/LocoDrivers.h
@@ -0,0 +1,64 @@
+// Copyright Stevens Studios, all rights reserved
+// Written by Thomas Stevens
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LocoDrivers.generated.h"
+
+UCLASS()
+class LITTLERAILWAYS_API ALocoDrivers : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALocoDrivers();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LocoParts", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* FrameBody;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LocoParts", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* LeftWheel1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LocoParts", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* LeftWheel2;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LocoParts", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* RightWheel1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LocoParts", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* RightWheel2;
+
+	//Variables
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TractionVariables")
+	float TractiveTorque;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TractionVariables")
+	float MaxSpeedKph;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TractionVariables")
+	TArray<UStaticMeshComponent*> wheelsToDrive;
+
+
+	//Functions
+	void ApplyTorque(int passedTorqueMultiplier);
+
+	void ApplyBreaks(int passedBrakeVal);
+
+	//References
+	class ALocoController* TrainRef;
+
+	void SetTrainPtr(ALocoController* TrainPrt);
+
+};
